c217d61008bd86028192b2e42c76fadc4352454d
MohammadAli DaneshSefatDoust <72104345+MohammadAliDaneshSefatDoust@users.noreply.github.com>
2022-04-22 14:08:40 +0430

    Add files via upload

--- 1- inputs.m
+++ 1- inputs.m
@@ -3,7 +3,7 @@ N_sample=32768;                   %number of samples
 Fs=100000000;                     %sampling rate
 Ts=(1/100000000);                 %TS=1/sampling rate
 t=(0:1:32767).*Ts;                %time
-Vin=0.1*sin(2*pi*Fin*t);            %input
+Vin=1*sin(2*pi*Fin*t);            %input
 Vin=Vin+0.1*Vin.^2-0.5*Vin.^3;
 Numbits=15;
 FSV=2*1;                          %Full Scale Voltage
